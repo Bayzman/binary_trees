a2f4fd7b27620042a4070bce36938488c3deb68a
Bayzman <bassmaninno102@gmail.com>
2024-06-06 20:29:30 -0700

    creates a binary tree node

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,7 +4,7 @@
  * binary_tree_node - creates a binary tree node
  * @parent: parent node
  * @value: int value
- * Return: created node or Null on failure
+ * Return: pointer to new node or Null on failure
  */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
